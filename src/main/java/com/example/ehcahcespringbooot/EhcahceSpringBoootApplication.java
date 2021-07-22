e5fd62225ab7c21252f419cab9420017428ad4f9
Franklin <forsonfranklin@gmail.com>
2021-07-22 12:34:57 +0000

    initial commit

---
+++ src/main/java/com/example/ehcahcespringbooot/EhcahceSpringBoootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehcahcespringbooot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcahceSpringBoootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EhcahceSpringBoootApplication.class, args);
+    }
+
+}
